500d0233061cc4695a530d96c687381a69507330
Natthakit <nonusae@gmail.com>
2017-07-31 18:26:34 +0700

    Implemeted destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -42,7 +42,18 @@ class PortfoliosController < ApplicationController
         format.json { render json: portfolios_path, status: :unprocessable_entity }
       end
     end
-    end   
+    end 
+
+
+
+    def destroy
+        @portfolio_item = Portfolio.find(params[:id]) 
+
+        @portfolio_item.destroy
+        respond_to do |format|
+          format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+        end
+    end  
 
     private
 
