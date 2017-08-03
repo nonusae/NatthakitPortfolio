d02b0d7cdd8e5d67596ebfd3a9cb8b54e937c9a1
Natthakit <nonusae@gmail.com>
2017-08-03 10:32:45 +0700

    Implemented default value

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,12 @@ class Portfolio < ApplicationRecord
 
     #Best practice to keep all logic for the model, Becuase the controller should only manage the data flow
     scope :ruby_on_rails_portfolio_items , ->  { where(subtitle: "Ruby On Rails") }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image ||= "http://via.placeholder.com/600x400"
+        self.thumb_image ||= "http://via.placeholder.com/350x200"
+    end
+
 end
