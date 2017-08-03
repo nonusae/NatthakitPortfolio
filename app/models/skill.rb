f73cbbc8eabc211bb9f6df605cbd275ee1989c65
Natthakit <nonusae@gmail.com>
2017-08-03 10:48:46 +0700

    Implemted concern for managing data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,10 @@
 class Skill < ApplicationRecord
+    include Placeholder #concern's module
     validates_presence_of :title, :percent_untilized
 
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.badge ||= Placeholder.image_generator(height:250, width:250)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802093249) do
+ActiveRecord::Schema.define(version: 20170803033833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170802093249) do
     t.integer "percent_untilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
