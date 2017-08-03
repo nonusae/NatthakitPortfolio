359e4167e48b264bebecc926c9a2d6a36a94d08f
Natthakit <nonusae@gmail.com>
2017-08-03 15:22:16 +0700

    Implemented Form configuration

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,6 +28,7 @@ class PortfoliosController < ApplicationController
 
     def new 
         @portfolio_item = Portfolio.new
+        3.times { @portfolio_item.technologies.build }
     end
 
     def edit
@@ -62,7 +63,7 @@ class PortfoliosController < ApplicationController
     private
 
         def portfolio_params
-          params.require(:portfolio).permit(:title, :subtitle, :body)
+          params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
         end 
 
 
