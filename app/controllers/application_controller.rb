92467496adad1c0ff979861a2505bcc4c32fda75
Natthakit <nonusae@gmail.com>
2017-08-07 20:58:41 +0700

    Dev tool initials commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = NatthakitViewTool::Renderer.copyright 'Natthakit Iewprasert', 'All rights reserved' 
+  end
+end
+
+module NatthakitViewTool
+    class Renderer
+        def self.copyright name, msg
+            "&copy; #{Time.new.year} | <b>#{name}</b> #{msg}".html_safe
+        end
+    end
 end
