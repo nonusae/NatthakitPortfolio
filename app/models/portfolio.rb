0ceeb3f4e81be625148411371d76327e9f86ae36
Natthakit <nonusae@gmail.com>
2017-08-10 14:29:10 +0700

    updated portfolios to include attr for position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -12,6 +12,10 @@ class Portfolio < ApplicationRecord
         where(subtitle: "Angular")
     end
 
+    def self.by_position
+        order("position ASC")
+    end
+
     #Best practice to keep all logic for the model, Becuase the controller should only manage the data flow
     scope :ruby_on_rails_portfolio_items , ->  { where(subtitle: "Ruby On Rails") }
 
@@ -21,5 +25,6 @@ class Portfolio < ApplicationRecord
         self.main_image ||= Placeholder.image_generator(height:600, width:400)
         self.thumb_image ||= Placeholder.image_generator(height:350, width:200)
     end
+
 
 end
