36a5a02e7fded82b14c9da07cdabb78b06c0ce21
Natthakit <nonusae@gmail.com>
2017-08-11 02:39:42 +0700

    Built out initial form and partial call from comment along with controller action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,35 +2,31 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
 
+  #Petergate's things
   access all: [:show, :index], 
          user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, 
          site_admin: :all
 
-  # GET /blogs
-  # GET /blogs.json
   def index
     @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My Portfolio blogs"
   end
 
-  # GET /blogs/1
-  # GET /blogs/1.json
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
+
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
@@ -45,8 +41,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /blogs/1
-  # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
@@ -59,8 +53,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
   def destroy
     @blog.destroy
     respond_to do |format|
@@ -80,12 +72,10 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.friendly.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
