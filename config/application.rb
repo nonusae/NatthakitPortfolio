afe4b436470555e776e66fcd9460c501a40b0d41
Natthakit <nonusae@gmail.com>
2017-08-11 09:12:57 +0700

    updated secret key base

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module NatthakitPortfolio
     config.generators.system_tests = nil
 
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
