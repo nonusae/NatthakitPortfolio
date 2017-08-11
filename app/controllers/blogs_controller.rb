df8ca8b01053bf07c5e946409b08df82ac6f9d22
Natthakit <nonusae@gmail.com>
2017-08-11 09:46:07 +0700

    Implemented custom scope for recent blogs and hide draft posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,8 +8,12 @@ class BlogsController < ApplicationController
          site_admin: :all
 
   def index
-    @blogs = Blog.page(params[:page]).per(5)
-    @page_title = "My Portfolio blogs"
+     if logged_in?(:site_admin)
+       @blogs = Blog.recent.page(params[:page]).per(5)
+     else
+       @blogs = Blog.published.recent.page(params[:page]).per(5)
+     end    
+     @page_title = "My Portfolio blogs"
   end
 
   def show
